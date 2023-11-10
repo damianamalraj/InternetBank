1d02ed428ca044a0e928d6ce9c934df419270c46
Damian Amalraj <damian.amalraj@hotmail.com>
2023-11-10 19:29:00 +0100

    Init InternetBank

---
+++ AdminFunctions.cs
@@ -0,0 +1,79 @@
+﻿using InternetBank.Data;
+using InternetBank.Models;
+using InternetBank.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBank
+{
+    internal static class AdminFunctions
+    {
+        public static void DoAdminTasks()
+        {
+            using (BankContext context = new BankContext())
+            {
+                Console.WriteLine("Current users in system:");
+                List<User> users = DbHelper.GetAllUsers(context);
+
+                foreach (User user in users)
+                {
+                    Console.WriteLine($"{user.Name}");
+                }
+
+                Console.WriteLine($"Total number of users = {users.Count()}");
+                Console.WriteLine("c to create new user");
+                Console.WriteLine("x to exit");
+
+                while (true)
+                {
+                    Console.Write("Enter command:");
+                    string command = Console.ReadLine();
+
+                    switch (command.ToLower())
+                    {
+                        case "c":
+                            CreateUser(context);
+                            break;
+                        case "x":
+                            return;
+                        default:
+                            Console.WriteLine($"Unknown command: {command}");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static void CreateUser(BankContext context)
+        {
+            Console.WriteLine("Create user");
+            Console.Write("Enter user name:");
+            string username = Console.ReadLine();
+
+            Random random = new Random();
+            string pin = random.Next(1000, 10000).ToString();
+
+
+            User newUser = new User()
+            {
+                Name = username,
+                Pin = pin,
+            };
+            bool success = DbHelper.AddUser(context, newUser);
+
+            if (success)
+            {
+                Console.WriteLine($"Created user {username} with pin {pin}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to create user with username {username}");
+            }
+        }
+
+
+    }
+}
---
+++ Data/BankContext.cs
@@ -0,0 +1,22 @@
+﻿using InternetBank.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBank.Data
+{
+    internal class BankContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Fill in connection string here!!
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
