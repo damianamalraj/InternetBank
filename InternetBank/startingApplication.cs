b93d01c11ea43cf474b3c9ee9144558c619d33c1
NpcRP <145152975+NpcRP@users.noreply.github.com>
2023-11-27 13:55:25 +0100

    Enhanced security and code enhancements
    -Implemented a lockout mechanism in startingApplication.cs to increase security by restricting access after multiple failed login attempts for both admin and user accounts.
    -Streamlined BankContext usage in UserFunctions.cs by passing it as a parameter to DoUserTasks method, eliminating the need to create a new instance within the method.
    -Smaller corrections and cleanup in the code here and ther

--- InternetBank/startingApplication.cs
+++ InternetBank/startingApplication.cs
@@ -1,52 +1,78 @@
 ﻿using System;
-using System.Media;
 using InternetBank.Data;
 
 namespace InternetBank
 {
-	public class startingApplication
-	{
-		public static void startProgram()
-		{
+    public class startingApplication
+    {
+        private static int failedLoginAttempts = 0;
+        private const int maxFailedAttempts = 3; // Maximum number of attempts
+        private const int timeoutDuration = 180; // Timeout duration in seconds
+
+        public static void startProgram()
+        {
             Console.Clear();
             Console.WriteLine("\t\tHi, Welcome to the Bank!");
             Console.WriteLine("\t\tPlease enter your login credentials to log in to the system.");
 
-            Console.Write("\t\t\n\t\tEnter Username: ");
-            string userName = Console.ReadLine();
-
-            Console.Write("\t\t\n\t\tYour pin code: ");
-            string pin = Console.ReadLine();
-
-            if (userName == "admin")
+            while (true)
             {
-                /*if (pin != "1234")
+                Console.Write("\t\t\n\t\tEnter Username: ");
+                string userName = Console.ReadLine();
+
+                Console.Write("\t\t\n\t\tYour pin code: ");
+                string pin = Console.ReadLine();
+
+                if (userName == "admin")
                 {
-                    Console.WriteLine("\t\tSorry, Wrong password!");
-                    return;
-                }*/
-
-                AdminFunctions.DoAdminTasks();
-                
-            }
-            using (BankContext context = new BankContext())
-            {
-                
-                var user = context.Users
-                            .Where(x => x.Name == userName)
-                            .Where(x => x.Pin == pin);
-
-                if (user != null)
+                    if (pin != "1234")
+                    {
+                        Console.WriteLine("\t\tSorry, Wrong password!");
+                        failedLoginAttempts++;
+                    }
+                    else
+                    {
+                        failedLoginAttempts = 0;
+                        AdminFunctions.DoAdminTasks();
+                        break;
+                    }
+                }
+                else
                 {
-
-                    UserFunctions.DoUserTasks(userName);
+                    using (BankContext context = new BankContext())
+                    {
+                        var user = context.Users.FirstOrDefault(x => x.Name == userName && x.Pin == pin);
+                        if (user != null)
+                        {
+                            failedLoginAttempts = 0;
+                            UserFunctions.DoUserTasks(userName, context);
+                            break;
+                        }
+                        else
+                        {
+                            failedLoginAttempts++;
+                        }
+                    }
                 }
 
+                // Lockout logic for both admin and user
+                if (failedLoginAttempts >= maxFailedAttempts)
+                {
+                    for (int remainingSeconds = timeoutDuration; remainingSeconds > 0; remainingSeconds--)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Too many incorrect attempts. Please wait for {remainingSeconds} seconds.");
+                        System.Threading.Thread.Sleep(1000); // Wait for 1 second
+                    }
+                    Console.Clear();
+                    Console.WriteLine("You may now attempt your login again. If further issues persist, consider contacting admin at admin@tigerconsole.se");
+                    failedLoginAttempts = 0; // Reset the counter after timeout
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect credentials. {maxFailedAttempts - failedLoginAttempts} attempts remaining.");
+                }
             }
-
         }
-
-        
     }
 }
-
